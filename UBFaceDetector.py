6469e3f927cdfc2cb8061ee4d6fe64d0e56b684f
Shrayesh Kanade <89949929+shraykanade@users.noreply.github.com>
2022-05-27 12:02:42 -0400

    Update UBFaceDetector.py

--- UBFaceDetector.py
+++ UBFaceDetector.py
@@ -27,41 +27,8 @@ import face_recognition
 Please do NOT add any imports. The allowed libraries are already imported for you.
 '''
 
-def load_images_from_folder(folder):
-    images = []
-    fname=[]
-    for filename in os.listdir(folder):
-        fname.append(filename)
-        img = cv2.imread(os.path.join(folder,filename),cv2.IMREAD_GRAYSCALE)
-        if img is not None:
-            images.append(img)
-    return images, fname
-
-
-
-
 def detect_faces(input_path: str) -> dict:
     result_list = []
-    count=[]
-    images, fname=load_images_from_folder(input_path)
-    print(fname)
-    print(len(fname))
-    number_of_images=len(images)
-    face = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_alt.xml')
-    print(face)
-    for i in range(number_of_images):
-        img = images[i]
-        faces = face.detectMultiScale(img,scaleFactor=1.1,minNeighbors=3, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
-
-        for x,y,w,h in faces:
-            face_boundary= {"iname": fname[i], "bbox": [int(x), int(y), int(w), int(h)]}
-            result_list.append(face_boundary)
-            cv2.rectangle(img,(x, y),(x + w, y + h),(0, 255, 0),2)
-    return result_list
-
-
-''''
-
     harr_file_path=cv2.data.haarcascades+'haarcascade_frontalface_alt.xml'
     harr_file_path=harr_file_path.replace('/','//')
     face_cascade = cv2.CascadeClassifier(harr_file_path)
@@ -73,7 +40,7 @@ def detect_faces(input_path: str) -> dict:
         detected_face = face_cascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=4, minSize=(20, 20),flags=cv2.CASCADE_SCALE_IMAGE)
         draw_rectangle(detected_face,img,img_name,result_list)
     return result_list
-'''
+
 '''
 K: number of clusters
 '''
